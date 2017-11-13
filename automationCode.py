db5be1271e44aae0bb17f3181160319c31a9e17a
faridprasaja <faridprasaja@live.com>
2017-11-13 10:08:37 +0700

    Testing Travis CI on Github

---
+++ automationCode.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 10:01:44 2017
+
+@author: solusi247
+"""
+
+import unittest
+
+class automationTest(unittest.TestCase):
+    def setup(self):
+        print "Setup Function"
+    
+    def test1(self):
+        print "Test 1"
+    
+    def test2(self):
+        print "Test 2"
+
+if __name__ == "__main__":
+    unittest.main()
